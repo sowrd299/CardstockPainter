3d540206759ae8731bbc41f83be1e7eaef4025dc
sowrd299 <ryan.ward46@gmail.com>
2019-12-06 12:56:19 -0800

    Adds XML based frame definitions

--- frame.py
+++ frame.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw
+from xml.etree import ElementTree
 from text_box import TextBox
+from os import path
 
 #returns fields derived from extant fields
 def get_derived_fields(item : dict, derived_fields : {str : "card => value"}):
@@ -18,6 +20,7 @@ class Frame():
         def save(self, file):
             self.img.save(file)
 
+
     # defaults
     size = (225,315)
     boarder_color = (0,0,0)
@@ -27,6 +30,52 @@ class Frame():
     def __init__(self, boxes : {str : TextBox}, derived_fields : {str : "card => value"}):
         self.boxes = boxes
         self.derived_fields = derived_fields
+
+
+    # creates a new frame, as defined by the given xml file
+    # TODO: handle getting data in different forms
+    # TODO: break this into multiple methods like a civilized programmer
+    @staticmethod
+    def open_from_xml(file_path : str):
+
+        frame = Frame(None, None)
+
+        tree = ElementTree.parse(file_path)
+        root = tree.getroot()
+
+        # create derived fields
+        derived_fields = dict()
+        for derived_field in root.iter("derived_field"):
+            derived_fields[derived_field.attrib["name"]] = lambda card : eval(derived_field.attrib["eq"].format(**card))
+
+        # create symbol sets
+        symbol_sets = dict()
+        for symbol_set in root.iter("symbol_set"):
+
+            # setup image loading
+            directory = symbol_set.attrib["dir"]
+            open_sym = lambda file : Image.open(path.join(directory, file))
+
+            # build the symbol set
+            #   using itter instead of find allows for symbol super and subsets to function automatically (though not efficiently)
+            symbol_sets[ symbol_set.attrib["name"] ] = { sym.attrib["name"] : open_sym(sym.attrib["file"]) for sym in symbol_set.iter("symbol") }
+
+        # create text boxes
+        text_boxes = dict()
+        frame_vars = { "w" : frame.size[0], "h" : frame.size[1], "bw" : frame.boarder_width }
+        eval_vars = lambda s : eval(s.format(**frame_vars))
+        for box in root.iter("text_box"):
+            text_boxes[ box.attrib["name"] ] = TextBox(
+                    (eval_vars(box.attrib["x"]), eval_vars(box.attrib["y"])),
+                    eval_vars(box.attrib["w"]),
+                    symbol_sets[box.attrib["symbols"]]
+            )
+
+        frame.boxes = text_boxes
+        frame.derived_fields = derived_fields
+
+        return frame
+
 
     # renders the given data in this frame
     def render(self, card : dict):
--- main.py
+++ main.py
@@ -7,36 +7,7 @@ from PIL import Image
 if __name__ == "__main__":
     print("Starting...")
 
-    # create derived fields
-    derived_fields = dict()
-    derived_fields["P/T"] = lambda card : "{0}/{1}".format(card["Power"], card["Toughness"]) if card["Power"] else ""
-
-    # create the symbols
-    symbols_paths = {
-        "{W}" : "W.svg.png",
-        "{U}" : "U.svg.png",
-        "{B}" : "B.svg.png",
-        "{R}" : "R.svg.png",
-        "{G}" : "G.svg.png",
-        "{T}" : "T.svg.png"
-    }
-
-    symbols = { s : Image.open("Symbols/" + p) for s,p in symbols_paths.items() }
-    short_symbols = { s[1] : i for s,i in symbols.items() } # without the brackets
-
-    # constants for creating the text boxes
-    boxes = dict()
-    bw = Frame.boarder_width
-    size = Frame.size
-    # the boxes themselves
-    boxes["Name"] = TextBox((bw+8,bw+8), size[0]-bw*2-16, symbols)
-    boxes["CMC"] = TextBox((bw+8, bw+32), size[0]-bw*2-16, short_symbols)
-    boxes["Type"] = TextBox((bw+8, size[1]/3-32), size[0]-bw*2-16, symbols)
-    boxes["Text"] = TextBox((bw+8, size[1]/3), size[0]-bw*2-16, symbols)
-    boxes["P/T"] = TextBox((3*size[0]/4, size[1]-32), 3*size[0]/4-bw, symbols)
-
-    # initialize the frame
-    frame = Frame(boxes, derived_fields)
+    frame = Frame.open_from_xml("FrameData.xml")
 
     # test CSV loader
     for card in csv_loader.cards_from("CardDesigns.csv"):
