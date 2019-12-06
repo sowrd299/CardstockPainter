d58afb3109fde8aff106783365ce204e8df244e1
sowrd299 <ryan.ward46@gmail.com>
2019-12-05 18:10:09 -0800

    A working first version of adding icons

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from PIL import Image, ImageDraw
 from tools import DictTree
+from collections import defaultdict
 import csv_loader
 
 class TextBox():
+
+    # tracks changing indexies at strings are edited
+    # allows code to reference consistent points accorss multiple versions
+    # deals with "orignal indexes", indexes before any changes to the string were made
+    class IndexMap():
+
+        def __init__(self):
+            self.map = defaultdict(int)
+
+        # logs that the given original index (and all subsequence original indexes) position has moved by <shift>
+        def add_shift(self, orig_i, shift):
+            self.map[orig_i] += shift
+            if self.map[orig_i] == 0:
+                del self.map[orig_i]
+
+        # returns the new index of the given character from the original string
+        def get_shifted_index(self, orig_i):
+            r = orig_i
+            for i,s in self.map.items():
+                if i <= orig_i:
+                    r += s
+            return r
 
     # defaults
     text_color = (0,0,0)
@@ -13,6 +36,7 @@ class TextBox():
         self.symbols = DictTree(symbols)
 
     # returns (<the text with symbol codes removed>, {(<symbol index> : <symbol>})
+    # TODO: add index mapping, and return symbols by original index
     def find_symbols(self, text):
 
         r_text = ""
@@ -39,6 +63,9 @@ class TextBox():
     # TODO: this is the nieve implementation
     # TODO: implement automatically creeping down text size
     def wrap_text(self, draw : ImageDraw.Draw, text : str):
+
+        index_map = self.IndexMap()
+
         wrapped_text = ""
 
         def under_width(ext = ''):
@@ -46,6 +73,7 @@ class TextBox():
 
         for i in range(len(text)):
             c = text[i]
+            len_added_chars = 0
             # actually keep building
             if under_width(c):
                 wrapped_text += c
@@ -60,34 +88,57 @@ class TextBox():
                     cprime = wrapped_text[-1]
                     wrapped_text = wrapped_text[:-1]
                     c = cprime + c
+                    i -= len(cprime) # allow i to continue tracking the first index in question
                 # do not put spaces at the start of lines, and don't put dashes before them
                 if c[0] == ' ':
                     wrapped_text += "\n"+c[1:]
+                    len_added_chars = 1 
                 else:
                     wrapped_text += "-\n" + c
-        return wrapped_text
+                    len_added_chars = 2
+                
+                index_map.add_shift(i, len_added_chars)
+
+        return (wrapped_text, index_map)
 
     # gets the pixel coordinate of the given index
     #   in the given text, after it would be drawn
-    def get_coord_in_text(draw, text, index):
+    def get_coord_in_text(self, draw, text, index):
+        # find the begining of the line the symbol in question is on
+        line_start_index = index
+        while line_start_index > 0 and text[line_start_index-1] != "\n":
+            line_start_index -= 1
 
+        x = draw.multiline_textsize(text[line_start_index:index])[0]
+        y = draw.multiline_textsize(text[:line_start_index-1])[1]
+        return (x,y) 
 
     # draw the given text onto the given box
-    def render(self, draw : ImageDraw.Draw, text : str):
-        # extract symbols
+    def render(self, img : Image.Image, text : str):
+
+        # setup
+        draw = ImageDraw.Draw(img)
+
+        # processing
         symbol_text, symbols = self.find_symbols(text)
-        # wrap the text
-        wrapped_text = self.wrap_text(draw, symbol_text)
+        wrapped_text, index_map_s_to_w = self.wrap_text(draw, symbol_text)
         # TODO: this is going to increase indexies...
-        # draw the text
+
+        # rendering
         draw.multiline_text(self.pos, wrapped_text, fill=self.text_color)
-        # draw symbols
+        char_size = draw.textsize("W") # assumes capital W largest letter
         for index, symbol in symbols.items():
-            pass # TODO
+            symbol_size = ( int(char_size[1] * symbol.size[0] / symbol.size[0]) , char_size[1])
+            s = symbol.resize(symbol_size)
+            local_coords = self.get_coord_in_text(draw, wrapped_text, index_map_s_to_w.get_shifted_index(index))
+            img_coords = ( int(self.pos[0] + local_coords[0]), int(self.pos[1] + local_coords[1]) )
+            img.paste( s,  img_coords )
+
 
 # a class to define a card frame layout
 class Frame():
 
+    # exists to abstract away pillow
     class RenderedCard():
         def __init__(self, img):
             self.img = img
@@ -107,7 +158,7 @@ class Frame():
     def render(self, card : dict):
 
         # setup
-        img = Image.new('RGB', self.size, color = self.boarder_color)
+        img = Image.new('RGBA', self.size, color = self.boarder_color)
         draw = ImageDraw.Draw(img)
 
         # create the boarder (by subtraction)
@@ -117,7 +168,7 @@ class Frame():
 
         # draw the text boxes
         for name,box in self.boxes.items():
-            box.render(draw, card[name])
+            box.render(img, card[name])
 
         # testing render 
         return self.RenderedCard(img)
@@ -130,17 +181,7 @@ if __name__ == "__main__":
     derived_fields = dict()
     derived_fields["P/T"] = lambda card : "{0}/{1}".format(card["Power"], card["Toughness"]) if card["Power"] else ""
 
-    # constants for creating the text boxes
-    boxes = dict()
-    bw = Frame.boarder_width
-    size = Frame.size
-    # the boxes themselves
-    boxes["Name"] = TextBox((bw+8,bw+8), size[0]-bw*2-16)
-    boxes["CMC"] = TextBox((bw+8, bw+32), size[0]-bw*2-16)
-    boxes["Type"] = TextBox((bw+8, size[1]/3-32), size[0]-bw*2-16)
-    boxes["Text"] = TextBox((bw+8, size[1]/3), size[0]-bw*2-16)
-    boxes["P/T"] = TextBox((3*size[0]/4, size[1]-32), 3*size[0]/4-bw)
-
+    # create the symbols
     symbols_paths = {
         "{W}" : "W.svg.png",
         "{U}" : "U.svg.png",
@@ -150,7 +191,19 @@ if __name__ == "__main__":
         "{T}" : "T.svg.png"
     }
 
-    symbols = { s : Image.open(p) for s,p in symbols_paths.items() }
+    symbols = { s : Image.open("Symbols/" + p) for s,p in symbols_paths.items() }
+    short_symbols = { s[1] : i for s,i in symbols.items() } # without the brackets
+
+    # constants for creating the text boxes
+    boxes = dict()
+    bw = Frame.boarder_width
+    size = Frame.size
+    # the boxes themselves
+    boxes["Name"] = TextBox((bw+8,bw+8), size[0]-bw*2-16, symbols)
+    boxes["CMC"] = TextBox((bw+8, bw+32), size[0]-bw*2-16, short_symbols)
+    boxes["Type"] = TextBox((bw+8, size[1]/3-32), size[0]-bw*2-16, symbols)
+    boxes["Text"] = TextBox((bw+8, size[1]/3), size[0]-bw*2-16, symbols)
+    boxes["P/T"] = TextBox((3*size[0]/4, size[1]-32), 3*size[0]/4-bw, symbols)
 
     # initialize the frame
     frame = Frame(boxes)
